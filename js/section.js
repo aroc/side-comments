b0957de18c3f5598318a6e7e247013de2e5f4f0d
Eric Anderson <eric@ericanderson.ca>
2014-05-23 13:48:58 -0500

    Documentation for Section class.

--- js/section.js
+++ js/section.js
@@ -2,6 +2,11 @@ _ = require('lodash');
 var Template = require('../templates/section.html');
 var CommentTemplate = require('../templates/comment.html');
 
+/**
+ * Creates a new Section object, which is responsible for managing a single comment section.
+ * @param {Object} $parentEl The jQuery object that represents the section.
+ * @param {Array} comments   The array of comments for this section. Optional.
+ */
 function Section( $parentEl, comments ) {
 	this.$parentEl = $parentEl;
 	this.comments = comments ? comments.comments : [];
@@ -10,11 +15,19 @@ function Section( $parentEl, comments ) {
 	this.render();
 }
 
+/**
+ * Callback for the comment button click event.
+ * @param {Object} event The event object.
+ */
 Section.prototype.addCommentClickCallback = function( event ) {
   event.preventDefault();
   this.toggleCommentForm(true);
 };
 
+/**
+ * Get the class to be used on the side comment section wrapper.
+ * @return {String} The class names to use.
+ */
 Section.prototype.commentClass = function() {
 	if (this.comments.length > 0) {
 		return 'has-comments';
@@ -23,6 +36,9 @@ Section.prototype.commentClass = function() {
 	}
 };
 
+/**
+ * Mark this section as selected.
+ */
 Section.prototype.select = function() {
 	this.$el.addClass('active');
 
@@ -31,15 +47,25 @@ Section.prototype.select = function() {
 	}
 };
 
+/**
+ * Deselect this section.
+ */
 Section.prototype.deselect = function() {
 	this.$el.removeClass('active');
 	this.toggleCommentForm(false);
 };
 
+/**
+ * Focus on the comment box in the comment form.
+ */
 Section.prototype.focusCommentBox = function() {
 	this.$el.find('.comment-box').get(0).focus();
 };
 
+/**
+ * Toggle showing or hiding the comment form for this section.
+ * @param  {Boolean} show Whether to show or hide the form.
+ */
 Section.prototype.toggleCommentForm = function( show ) {
   if (this.comments.length > 0) {
     this.$el.find('.add-comment').toggleClass('hide', show);
@@ -53,6 +79,9 @@ Section.prototype.toggleCommentForm = function( show ) {
   }
 };
 
+/**
+ * Render this section into the DOM.
+ */
 Section.prototype.render = function() {
 	var data = {
 	  commentTemplate: CommentTemplate,
@@ -62,6 +91,9 @@ Section.prototype.render = function() {
 	this.$el = $(_.template(Template, data)).appendTo(this.$parentEl);
 };
 
+/**
+ * Desttroy this Section object. Generally meaning unbind events.
+ */
 Section.prototype.destroy = function() {
 	this.$parentEl.off();
 }
--- side-comments.js
+++ side-comments.js
@@ -9067,6 +9067,11 @@ _ = require('lodash');
 var Template = require('../templates/section.html');
 var CommentTemplate = require('../templates/comment.html');
 
+/**
+ * Creates a new Section object, which is responsible for managing a single comment section.
+ * @param {Object} $parentEl The jQuery object that represents the section.
+ * @param {Array} comments   The array of comments for this section. Optional.
+ */
 function Section( $parentEl, comments ) {
 	this.$parentEl = $parentEl;
 	this.comments = comments ? comments.comments : [];
@@ -9075,11 +9080,19 @@ function Section( $parentEl, comments ) {
 	this.render();
 }
 
+/**
+ * Callback for the comment button click event.
+ * @param {Object} event The event object.
+ */
 Section.prototype.addCommentClickCallback = function( event ) {
   event.preventDefault();
   this.toggleCommentForm(true);
 };
 
+/**
+ * Get the class to be used on the side comment section wrapper.
+ * @return {String} The class names to use.
+ */
 Section.prototype.commentClass = function() {
 	if (this.comments.length > 0) {
 		return 'has-comments';
@@ -9088,6 +9101,9 @@ Section.prototype.commentClass = function() {
 	}
 };
 
+/**
+ * Mark this section as selected.
+ */
 Section.prototype.select = function() {
 	this.$el.addClass('active');
 
@@ -9096,15 +9112,25 @@ Section.prototype.select = function() {
 	}
 };
 
+/**
+ * Deselect this section.
+ */
 Section.prototype.deselect = function() {
 	this.$el.removeClass('active');
 	this.toggleCommentForm(false);
 };
 
+/**
+ * Focus on the comment box in the comment form.
+ */
 Section.prototype.focusCommentBox = function() {
 	this.$el.find('.comment-box').get(0).focus();
 };
 
+/**
+ * Toggle showing or hiding the comment form for this section.
+ * @param  {Boolean} show Whether to show or hide the form.
+ */
 Section.prototype.toggleCommentForm = function( show ) {
   if (this.comments.length > 0) {
     this.$el.find('.add-comment').toggleClass('hide', show);
@@ -9118,6 +9144,9 @@ Section.prototype.toggleCommentForm = function( show ) {
   }
 };
 
+/**
+ * Render this section into the DOM.
+ */
 Section.prototype.render = function() {
 	var data = {
 	  commentTemplate: CommentTemplate,
@@ -9127,6 +9156,9 @@ Section.prototype.render = function() {
 	this.$el = $(_.template(Template, data)).appendTo(this.$parentEl);
 };
 
+/**
+ * Desttroy this Section object. Generally meaning unbind events.
+ */
 Section.prototype.destroy = function() {
 	this.$parentEl.off();
 }
