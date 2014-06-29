e40a275c0fad3c7b3bf63bcf7ae82b93bda07502
Eric Anderson <eric@ericanderson.ca>
2014-06-29 00:25:18 -0600

    Don't have body tap binding to close comments for mobile.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ var _ = require('./vendor/lodash-custom.js');
 var Section = require('./section.js');
 var Emitter = require('emitter');
 var eventPipe = new Emitter;
-var mobileCheck = require('./helpers/mobile-check.js');
 
 /**
  * Creates a new SideComments instance.
@@ -21,7 +20,6 @@ function SideComments( el, currentUser, existingComments ) {
   this.$el = $(el);
   this.$body = $('body');
   this.eventPipe = eventPipe;
-  this.clickEventName = mobileCheck() ? 'tap' : 'click';
 
   this.currentUser = _.clone(currentUser) || null;
   this.existingComments = _.cloneDeep(existingComments) || [];
@@ -36,7 +34,7 @@ function SideComments( el, currentUser, existingComments ) {
   this.eventPipe.on('commentPosted', _.bind(this.commentPosted, this));
   this.eventPipe.on('commentDeleted', _.bind(this.commentDeleted, this));
   this.eventPipe.on('addCommentAttempted', _.bind(this.addCommentAttempted, this));
-  this.$body.on(this.clickEventName, _.bind(this.bodyClick, this));
+  this.$body.on('click', _.bind(this.bodyClick, this));
   this.initialize(this.existingComments);
 }
 
